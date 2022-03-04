9ec34ab03ece80f394e327a5e2e2d2df5e67cba0
Null <88463490+8sp@users.noreply.github.com>
2022-03-04 08:50:17 -0800

    Update gh-checker.py

--- src/gh-checker.py
+++ src/gh-checker.py
@@ -1,28 +1,33 @@
-# MODULES
+# LIBRARIES
+
 import os
 import colorama
 from os import system
 from time import sleep
 from colorama import Fore
 colorama.init(autoreset=True)
-system("title " + "Gh-Checker - By Null - @ENTRYSQUAD")
+system("title " + "Gh-Checker - By Null")
 try:
     import requests
 except:
     os.system("pip install requests")
     
-#LOGO
-logo = """     
+# LOGO
+
+logo =f"""     
    _   _       _ _ 
   | \ | |_   _| | |
   |  \| | | | | | |
   | |\  | |_| | | |
-  |_| \_|\__,_|_|_| (v1.0)
+  |_| \_|\__,_|_|_| {Fore.WHITE}(v1.0){Fore.RESET}
 """
 print(Fore.CYAN+logo)
 input(f"[{Fore.CYAN}i{Fore.RESET}] Press ENTER, When You Have [users.txt] File")
 u = open("users.txt","r")
 z = 0
+
+# WHILE LOOP
+
 while 1:
     user = u.readline().split("\n")[0]
     r = requests.get(f"https://github.com/{user}").status_code
